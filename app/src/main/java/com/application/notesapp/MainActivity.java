c867fe35ece1559e337ba5268f624e8c938fbc0d
pawangorai <pawankumar17264@gmail.com>
2020-01-23 00:52:22 +0530

    fixed bugs

--- app/src/main/java/com/application/notesapp/MainActivity.java
+++ app/src/main/java/com/application/notesapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.application.notesapp;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.application.notesapp.adapters.NotesAdapter;
 import com.application.notesapp.callbacks.NoteEventListener;
@@ -11,31 +20,16 @@ import com.application.notesapp.db.notesDoa;
 import com.application.notesapp.model.Note;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static com.application.notesapp.editNoteActivity.NOTE_EXTRA_key;
 
 public class MainActivity extends AppCompatActivity implements NoteEventListener {
-    private static final String TAG ="MainActivity";
+    private static final String TAG = "MainActivity";
     private RecyclerView recyclerView;
     public ArrayList<Note> notes;
-    private NotesAdapter adapter;
     private notesDoa dao;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements NoteEventListener
         setSupportActionBar(toolbar);
         recyclerView = findViewById(R.id.notes_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        dao = notesDB.getInstance(this).notesDao();
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -54,29 +49,42 @@ public class MainActivity extends AppCompatActivity implements NoteEventListener
                 //add new note
                 onAddNewNote();
             }
-
-
         });
-      dao = notesDB.getInstance(this).notesDao();
+
     }
 
     public void loadNote() {
         this.notes = new ArrayList<>();
-        List<Note> list =dao.getNotes();
+        List<Note> list = dao.getNotes();
+
         this.notes.addAll(list);
-        this.adapter = new NotesAdapter(this , notes);
-        this.adapter.setListener(this);
-
-
+        NotesAdapter adapter = new NotesAdapter(this, notes);
+        adapter.setListener(this);
         this.recyclerView.setAdapter(adapter);
     }
 
     private void onAddNewNote() {
-
-        startActivity(new Intent(this, editNoteActivity.class));
-
+        Intent edit = new Intent(this, editNoteActivity.class);
+        edit.putExtra("EDIT", false);
+        startActivity(edit);
     }
 
+
+    @Override
+    public void onNoteClick(Note note, long id) {
+        Intent edit = new Intent(this, editNoteActivity.class);
+        edit.putExtra("EDIT", true);
+        edit.putExtra(NOTE_EXTRA_key, id);
+        startActivity(edit);
+    }
+
+    @Override
+    public void onNoteLongClick(final Note note) {
+
+        Log.d(TAG, "OnNoteClick" + note.getId());
+        Toast.makeText(this, "LongPress", Toast.LENGTH_SHORT).show();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,28 +110,9 @@ public class MainActivity extends AppCompatActivity implements NoteEventListener
 
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         loadNote();
-
-
-    }
-
-    @Override
-    public void onNoteClick(Note note) {
-
-Intent edit =new Intent(this,editNoteActivity.class);
-edit.putExtra(NOTE_EXTRA_key ,note.getId());
-startActivity(edit);
-    }
-
-    @Override
-    public void onNoteLongClick(final Note note)
-    {
-
-      Log.d( TAG, "OnNoteClick"+note.getId());
-
-
     }
 }
 
--- app/src/main/java/com/application/notesapp/editNoteActivity.java
+++ app/src/main/java/com/application/notesapp/editNoteActivity.java
@@ -1,11 +1,13 @@
 package com.application.notesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.application.notesapp.db.notesDB;
 import com.application.notesapp.db.notesDoa;
@@ -14,52 +16,65 @@ import com.application.notesapp.model.Note;
 import java.util.Date;
 
 public class editNoteActivity extends AppCompatActivity {
-public EditText input_note;
-private notesDoa dao;
-private Note temp;
-public static final String NOTE_EXTRA_key = "note_id";
+    public EditText input_note;
+    private notesDoa dao;
+    private Note temp;
+    public static final String NOTE_EXTRA_key = "note_id";
+    private boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
         input_note = findViewById(R.id.input_note);
-        dao =notesDB.getInstance(this).notesDao();
-         if (getIntent().getExtras()!=null){
-             int id =getIntent().getExtras().getInt(NOTE_EXTRA_key,0);
-             temp=dao.getNoteById(id);
+        dao = notesDB.getInstance(this).notesDao();
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
 
-             input_note.setText(temp.getNote_text());
-         }
-         else temp =new Note();
+        if (bundle != null) {
+            status = bundle.getBoolean("EDIT", false);
+        }
+        long current_note_id = 0;
+        if (bundle != null) {
+            current_note_id = bundle.getLong(NOTE_EXTRA_key);
+        }
+        if (status) {
+            //    int id = getIntent().getExtras().getInt(NOTE_EXTRA_key, 0);
+            temp = dao.getNoteById((int) current_note_id);
+            input_note.setText(temp.getNote_text());
+        } else {
+            temp = new Note();
+        }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.edit_note_menu,menu);
-    return  super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.edit_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id==R.id.save_note)
+        if (id == R.id.save_note)
             onSaveNote();
         return super.onOptionsItemSelected(item);
     }
 
     private void onSaveNote() {
         String text = input_note.getText().toString();
-        if(!text.isEmpty())
-        {
-            long date =new Date().getTime();
-temp.setNote_date(date);
-temp.setNote_text(text);
-if (temp.getId() == -1)
-
-    dao.insertNote(temp);
-else dao.updateNote(temp);
-
-            finish();//returns
+        if (!text.isEmpty()) {
+            long date = new Date().getTime();
+            temp.setNote_date(date);
+            temp.setNote_text(text);
+            if (status)
+                dao.updateNote(temp);
+            else {
+                long id = notesDB.getInstance(this).notesDao().insertNote(temp);
+                temp.setId(id);
+                Log.e("Note_ID", String.valueOf(id));
+            }
+            finish();
         }
     }
 }
