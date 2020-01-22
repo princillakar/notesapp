39bdf5ef3360ea55caef4e6e57bef664a40ca0c5
Avinaba-Ray <avinabaray@gmail.com>
2020-01-22 19:22:03 +0530

    Previous Working Version

--- app/src/main/java/com/application/notesapp/adapters/NotesAdapter.java
+++ app/src/main/java/com/application/notesapp/adapters/NotesAdapter.java
@@ -27,7 +27,7 @@ public Context context;
     @Override
     public NoteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View v = LayoutInflater.from(context).inflate(R.layout.note_layout, parent ,false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_layout, parent ,false);
 
 
         return new NoteHolder(v);
